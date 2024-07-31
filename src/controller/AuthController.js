c813b842494f19ce75101772909785f7353dbb97
Abhinav Mathur <mathur.abhinav1108@gmail.com>
2024-07-31 22:33:11 +0530

    AppError

--- src/controller/AuthController.js
+++ src/controller/AuthController.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
+const AppError = require('../utils/AppError');
 const User = require('../db/user');
 
 exports.login = catchAsync(async (req, res, next) => {
